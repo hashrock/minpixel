4c49dd8dd51d7217fa62737d21a3313a91ab152c
hashrock <hashedrock@gmail.com>
2017-04-05 18:04:19 +0900

    update to Webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
 var path = require("path")
-
 module.exports = {
   entry: ['./src/main.ts'],
   output: {
-    path: 'dist',
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: [ '.ts', '.js']
   },
   module: {
     loaders: [
-      {test: /\.ts(x?)$/, loader: 'ts-loader'}
-    ],
-    postLoaders: [
+      {test: /\.ts(x?)$/, loader: 'ts-loader'},
       {
         include: path.resolve(__dirname, 'node_modules/pixi.js'),
-        loader: 'ify'
+        loader: 'ify-loader',
+        enforce: "post"
       }
     ]
   }
